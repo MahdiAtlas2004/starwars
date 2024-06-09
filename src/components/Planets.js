5dda47c74336d2ea870091efdd8c2c076cc37c41
Mahdi Atlas <mahdiatlas2004@gmail.com>
2024-06-09 18:05:22 +0430

    config object added to project

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -9,24 +9,27 @@ const fetchPlanets = async () => {
 };
 
 const Planets = () => {
-  const { data, status } = useQuery("planets", fetchPlanets);
+  //The third argument is for config object
+  const { data, status } = useQuery("planets", fetchPlanets, {
+    staleTime: 0,
+    //cacheTime: 10,
+    onSuccess: () => console.log("data fetched with no problemo"),
+  });
   console.log(data);
   return (
     <div>
       <h2>Planets</h2>
       {/* <p>{status}</p> */}
 
-      {status === 'loading' && (
-        <div>Loading data...</div>
-      )}
+      {status === "loading" && <div>Loading data...</div>}
 
-      {status === 'error' && (
-        <div>Error fetching data!</div>
-      )}
+      {status === "error" && <div>Error fetching data!</div>}
 
-      {status === 'success' && (
+      {status === "success" && (
         <div>
-          {data.results.map(planet => <Planet key={planet.name} planet={planet}/>)}
+          {data.results.map((planet) => (
+            <Planet key={planet.name} planet={planet} />
+          ))}
         </div>
       )}
     </div>
